cf258e69f24043e05151dff6044ee243e41f4764
Ram Kishor <ram@omniesolutions.com>
2024-05-18 09:07:27 +0530

    ok

--- src/main/java/com/brijframework/content/global/model/UIGlobalTagGroup.java
+++ src/main/java/com/brijframework/content/global/model/UIGlobalTagGroup.java
@@ -6,4 +6,13 @@ public class UIGlobalTagGroup extends UIGlobalItem implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	private Long subCategoryId;
+
+	public Long getSubCategoryId() {
+		return subCategoryId;
+	}
+
+	public void setSubCategoryId(Long subCategoryId) {
+		this.subCategoryId = subCategoryId;
+	}
 }
